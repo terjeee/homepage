cf5517bb4d611363cd1bcf5c4bccff35494068a1
terjeee <96795863+terjeee@users.noreply.github.com>
2022-09-22 04:54:48 +0200

    rusken

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import Nav from "./components/Nav/Nav";
+
+import "./css/global/app.scss";
+
 function App() {
-  return <div></div>;
+  return (
+    <div className="app">
+      <Nav />
+    </div>
+  );
 }
 
 export default App;
