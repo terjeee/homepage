d5089ea84a8dee3e31b177cec8796e0ee56961b1
terjeee <96795863+terjeee@users.noreply.github.com>
2022-09-24 00:17:17 +0200

    <Routes />

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
-import Nav from "./components/Nav/Nav";
+import { Routes, Route, Navigate } from "react-router-dom";
+
+import Nav from "./layout/Nav/Nav";
+import Home from "./routes/Home/Home";
+import Experiment from "./routes/Experiment/Experiment";
+import Footer from "./layout/Footer/Footer";
 
 import "./css/global/app.scss";
 
@@ -6,6 +11,12 @@ function App() {
   return (
     <div className="app">
       <Nav />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sandbox" element={<Experiment />} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+      <Footer />
     </div>
   );
 }
