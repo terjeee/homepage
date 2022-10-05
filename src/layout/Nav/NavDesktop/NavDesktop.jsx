b4299f96308823b77bef1dd6c125c5c821346a35
terjeee <96795863+terjeee@users.noreply.github.com>
2022-10-05 04:42:53 +0200

    nav

--- src/layout/Nav/NavDesktop/NavDesktop.jsx
+++ src/layout/Nav/NavDesktop/NavDesktop.jsx
@@ -1,12 +1,16 @@
-import { Button } from "@mui/material";
-
 import css from "./NavDesktop.module.scss";
 
 export default function NavBar() {
   return (
     <nav className={css.nav}>
       <div className={css.container}>
-        <Button variant="contained">Log In</Button>
+        <div>
+          <h2>
+            {"<"}
+            <span>terje</span>
+            {" />"}
+          </h2>
+        </div>
       </div>
     </nav>
   );
