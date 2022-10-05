81579f860dc857b7a894c989f4e1a1805757b5c6
terjeee <96795863+terjeee@users.noreply.github.com>
2022-10-05 05:35:38 +0200

    prosjekter from <projects />

---
+++ src/assets/PROJECTS.jsx
@@ -0,0 +1,44 @@
+export const PROJECTS = [
+  {
+    id: 1,
+    link: "https://terjeee.github.io/2022-09-04-rest-countries-api/",
+    linkRepo: "https://github.com/terjeee/2022-09-04-rest-countries-api",
+    title: "Countries API",
+    text: "Pulls data from an API, with each country having a separate route with more details.",
+  },
+  {
+    id: 2,
+    link: "https://terjeee.github.io/2022-08-21-bank-landing-page/",
+    linkRepo: "https://github.com/terjeee/2022-08-21-bank-landing-page",
+    title: "Landing Page (Easybank)",
+    text: "Dummy landing page for a bank.",
+  },
+  {
+    id: 3,
+    link: "https://terjeee.github.io/2022-08-14-ip-address-tracker/",
+    linkRepo: "https://github.com/terjeee/2022-08-14-ip-address-tracker",
+    title: "IP Address Tracker",
+    text: "Shows the location of an IP address on a map.",
+  },
+  {
+    id: 4,
+    link: " https://terjeee.github.io/2022-08-10-calculator-app/",
+    linkRepo: "https://github.com/terjeee/2022-08-10-calculator-app",
+    title: "Calculator",
+    text: ":)",
+  },
+  {
+    id: 5,
+    link: "https://terjeee.github.io/2022.05.27-Advice-Generator/",
+    linkRepo: "https://github.com/terjeee/2022.05.27-Advice-Generator",
+    title: "Advice Generator",
+    text: "Pulls advices fron an API.",
+  },
+  {
+    id: 6,
+    link: "https://terjeee.github.io/2022.02.12-GuessNumber/",
+    linkRepo: "https://github.com/terjeee/2022.02.12-GuessNumber",
+    title: "Guess the Number",
+    text: "NB: Skrev bare JS.",
+  },
+];
