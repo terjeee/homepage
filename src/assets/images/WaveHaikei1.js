c8bcac701c563b3ea925473f7362e6644fa19e79
terjeee <96795863+terjeee@users.noreply.github.com>
2022-10-06 16:12:23 +0200

    +haikei waves

---
+++ src/assets/images/WaveHaikei1.js
@@ -0,0 +1,20 @@
+import * as React from "react";
+
+const SvgWaveHaikei3 = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="-7.5 265.82 915 53.68"
+    role="img"
+    {...props}
+  >
+    <path
+      d="m0 312 30-6.8c30-6.9 90-20.5 150-27.2 60-6.7 120-6.3 180 1.8 60 8.2 120 24.2 180 27 60 2.9 120-7.5 180-11s120-.1 150 1.5l30 1.7"
+      fill="none"
+      strokeLinecap="round"
+      stroke="#072ac8"
+      strokeWidth={5}
+    />
+  </svg>
+);
+
+export default SvgWaveHaikei3;
---
+++ src/assets/images/WaveHaikei2.js
@@ -0,0 +1,20 @@
+import * as React from "react";
+
+const SvgWaveHaikei4 = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="-7.5 294.98 915 40.52"
+    role="img"
+    {...props}
+  >
+    <path
+      d="m0 328 30-.5c30-.5 90-1.5 150-5.7 60-4.1 120-11.5 180-15.5s120-4.6 180-3c60 1.7 120 5.7 180 8 60 2.4 120 3 150 3.4l30 .3"
+      fill="none"
+      strokeLinecap="round"
+      stroke="#072ac8"
+      strokeWidth={5}
+    />
+  </svg>
+);
+
+export default SvgWaveHaikei4;
