e6b9730043cc39236056a40942504800993069be
terjeee <96795863+terjeee@users.noreply.github.com>
2022-10-06 16:12:12 +0200

    +- icons

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -1,8 +1,7 @@
 export { default as IconJs } from "./IconJs";
-export { default as IconMui } from "./IconMui";
 export { default as IconRedux } from "./IconRedux";
 export { default as IconRouter } from "./IconRouter";
 export { default as IconReact } from "./IconReact";
 export { default as IconSass } from "./IconSass";
-export { default as GithubRepo } from "./GithubRepository";
-export { default as Dots } from "./DraggableDots";
+export { default as IconRepository } from "./IconRepository";
+export { default as IconDots } from "./IconDots";
