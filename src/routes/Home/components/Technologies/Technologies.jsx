c4538366b79cab68f20adbf0054390c0649ac43e
terjeee <96795863+terjeee@users.noreply.github.com>
2022-10-11 05:28:42 +0200

    technologies

---
+++ src/routes/Home/components/Technologies/Technologies.jsx
@@ -0,0 +1,12 @@
+import TechnologiesList from "./components/TechnologiesList";
+import css from "./Technologies.module.scss";
+
+export default function Technologies() {
+  return (
+    <section className={css.technologies}>
+      <div className={css.container}>
+        <TechnologiesList />
+      </div>
+    </section>
+  );
+}
---
+++ src/routes/Home/components/Technologies/components/TechnologiesList.jsx
@@ -0,0 +1,25 @@
+import { IconJs, IconSass, IconReact } from "../../../../../assets/icons/index";
+import { IconRedux, IconRouter } from "../../../../../assets/icons/index";
+import css from "./TechnologiesList.module.scss";
+
+export default function TechnologiesList() {
+  return (
+    <ul className={css.icons}>
+      <li>
+        <IconJs />
+      </li>
+      <li>
+        <IconSass />
+      </li>
+      <li>
+        <IconReact />
+      </li>
+      <li>
+        <IconRedux />
+      </li>
+      <li>
+        <IconRouter />
+      </li>
+    </ul>
+  );
+}
