f160571798f59683b8d2ffb5fe33ac3a24396976
terjeee <96795863+terjeee@users.noreply.github.com>
2022-10-11 02:22:52 +0200

    waves

---
+++ src/assets/images/WaveHaikei.js
@@ -0,0 +1,9 @@
+import * as React from "react";
+
+const SvgWaveHaikei1Cropped = (props) => (
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="-5 294 910 10" role="img" {...props}>
+    <path d="M0 299h900" fill="none" strokeLinecap="round" stroke="#000" strokeWidth={5} />
+  </svg>
+);
+
+export default SvgWaveHaikei1Cropped;
