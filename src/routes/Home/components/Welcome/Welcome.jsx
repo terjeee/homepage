00ea449c4ecf39126beaf7f8b539434b588fa537
terjeee <96795863+terjeee@users.noreply.github.com>
2022-10-11 05:22:20 +0200

    welcome

---
+++ src/routes/Home/components/Welcome/Welcome.jsx
@@ -0,0 +1,45 @@
+import { TypeAnimation } from "react-type-animation";
+import TechnologiesList from "../Technologies/components/TechnologiesList";
+import css from "./Welcome.module.scss";
+
+export default function Welcome() {
+  return (
+    <header className={css.welcome}>
+      <div className={css.container}>
+        <div className={css.text}>
+          <TypeAnimation
+            sequence={[
+              "Hei",
+              3000,
+              "Bonjour",
+              3000,
+              "こんにちは",
+              3000,
+              "Ciao",
+              3000,
+              "你好",
+              3000,
+              "Guten Tag",
+              3000,
+              "नमस्ते",
+              3000,
+            ]}
+            wrapper="h1"
+            speed="1"
+            cursor={true}
+            repeat={Infinity}
+            style={{
+              fontSize: "3rem",
+              fontFamily: "inherit",
+              color: "inherit",
+              letterSpacing: "2.5px",
+            }}
+          />
+        </div>
+        <div className={css.technologies}>
+          <TechnologiesList />
+        </div>
+      </div>
+    </header>
+  );
+}
