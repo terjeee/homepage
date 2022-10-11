6d9d9b4c7938c7e79908654968d72f34b1908a9c
terjeee <96795863+terjeee@users.noreply.github.com>
2022-10-11 05:28:54 +0200

    projects

--- src/routes/Home/components/Projects/Projects.jsx
+++ src/routes/Home/components/Projects/Projects.jsx
@@ -1,11 +1,25 @@
+import { PROJECTS } from "../../../../assets/PROJECTS";
+
 import ProjectSingle from "./components/ProjectSingle";
 import css from "./Projects.module.scss";
 
 export default function Projects() {
   return (
-    <section className={css.projects}>
+    <section className={css.projects} id="projects">
       <div className={css.container}>
-        <ProjectSingle />
+        <h2>PROJECTS</h2>
+        <ul className={css.list}>
+          {PROJECTS.map((el) => (
+            <ProjectSingle
+              key={el.id}
+              title={el.title}
+              text={el.text}
+              link={el.link}
+              linkRepo={el.linkRepo}
+              colorDot={el.colorDot}
+            />
+          ))}
+        </ul>
       </div>
     </section>
   );
--- src/routes/Home/components/Projects/components/ProjectSingle.jsx
+++ src/routes/Home/components/Projects/components/ProjectSingle.jsx
@@ -1,18 +1,25 @@
-import { GithubRepo } from "../../../../../assets/icons/index";
+import { IconRepository, IconDots } from "../../../../../assets/icons/index";
 import css from "./ProjectSingle.module.scss";
 
 export default function ProjectSingle(props) {
   return (
     <li className={css.project}>
       <div className={css.title}>
-        <GithubRepo />
-        <a href={props.link} target="_blank" rel="noreferrer">
-          {props.title}
-        </a>
-        <span className={css.label}>Public</span>
+        <div>
+          <a href={props.linkRepo} target="_blank" rel="noreferrer" className={css.linkRepo}>
+            <IconRepository />
+          </a>
+          <a href={props.link} target="_blank" rel="noreferrer" className={css.link}>
+            {props.title}
+          </a>
+          <span className={css.label}>Public</span>
+        </div>
+        <div className={css.dots}>
+          <IconDots />
+        </div>
       </div>
       <div>
-        <span className={css.dot}></span>
+        <span className={css.dot} style={{ backgroundColor: props.colorDot }}></span>
         <p className={css.text}>{props.text}</p>
       </div>
     </li>
