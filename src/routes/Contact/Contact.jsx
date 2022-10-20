a8fa3bd9ff50fab9db92c061c36d5428f93bc24f
terjeee <96795863+terjeee@users.noreply.github.com>
2022-10-20 02:33:14 +0200

    +contact functionality

--- src/routes/Contact/Contact.jsx
+++ src/routes/Contact/Contact.jsx
@@ -1,3 +1,50 @@
+import { useRef, useState } from "react";
+import emailjs from "emailjs-com";
+
+import css from "./Contact.module.scss";
+
 export default function Contact() {
-  return <section>Contact :)</section>;
+  const formData = useRef();
+  const [formResponse, setFormResponse] = useState(null);
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    // emailjs
+    //   .sendForm(
+    //     "service_contactForm",
+    //     "template_70fzocf",
+    //     formData.current,
+    //     "6hbDSK_0uSjg0vdP5"
+    //   )
+    //   .then(
+    //     (result) => {
+    //       console.log(result);
+    //       setFormResponse("Message Sent!");
+    //     },
+    //     (error) => {
+    //       console.log(error.text);
+    //       setFormResponse("Something went wrong! Try again.");
+    //     }
+    //   );
+
+    formData.current.reset();
+  };
+
+  return (
+    <section className={css.page}>
+      <div className={css.container}>
+        <form className={css.form} ref={formData} onSubmit={sendEmail}>
+          <label htmlFor="formName">Name</label>
+          <input type="text" name="formName" id="formName" autoFocus />
+          <label htmlFor="formEmail">Email</label>
+          <input type="email" name="formEmail" id="formEmail" />
+          <label htmlFor="formMsg">Message</label>
+          <input type="text" name="formMsg" id="formMsg" />
+          <button>Send</button>
+        </form>
+        {formResponse && <p>{formResponse}</p>}
+      </div>
+    </section>
+  );
 }
