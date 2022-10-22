af74282f99e00917a11e4a708092def4544edd8f
terjeee <96795863+terjeee@users.noreply.github.com>
2022-10-22 02:16:13 +0200

    shit funker!

--- src/routes/Contact/Contact.jsx
+++ src/routes/Contact/Contact.jsx
@@ -3,24 +3,23 @@ import emailjs from "emailjs-com";
 
 import css from "./Contact.module.scss";
 
+const regexEmail = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
 export default function Contact() {
-  const [isTyping, setIsTyping] = useState(false);
-
   const [email, setEmail] = useState("");
-  const [emailValid, setEmailValid] = useState(false);
-
   const [message, setMessage] = useState("");
+  const [emailValid, setEmailValid] = useState(false);
   const [messageValid, setMessageValid] = useState(false);
 
-  const btnSendEnabled = emailValid && messageValid;
+  const formValid =
+    email.length > 0 && regexEmail.test(email) && message.length > 10 && message.length < 250;
 
   useEffect(() => {
     setEmailValid(true);
-    const regex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
     const timeoutEmail = setTimeout(() => {
-      if (email.length > 0 && !regex.test(email)) return setEmailValid(false);
+      if (email.length > 0 && !regexEmail.test(email)) return setEmailValid(false);
     }, 750);
-    return () => clearTimeout(timeoutEmail); // return (in useEffect) = clean-up (før main func)
+    return () => clearTimeout(timeoutEmail); // return (i useEffect) = clean-up
   }, [email]);
 
   useEffect(() => {
@@ -29,7 +28,7 @@ export default function Contact() {
       if (message.length !== 0 && (message.length < 10 || message.length > 100))
         return setMessageValid(false);
     }, 750);
-    return () => clearTimeout(timeoutMessage); // return (in useEffect) = clean-up
+    return () => clearTimeout(timeoutMessage); // return (i useEffect) = clean-up
   }, [message]);
 
   const sendEmail = (e) => {
@@ -60,23 +59,27 @@ export default function Contact() {
     <section className={css.page}>
       <div className={css.container}>
         <form className={css.form} onSubmit={sendEmail}>
-          <label htmlFor="formEmail">Email</label>
-          <input
-            type="email"
-            name="formEmail"
-            id="formEmail"
-            onChange={(event) => setEmail(event.target.value)}
-          />
-          <label htmlFor="formMsg">Message</label>
-          <input
-            type="text"
-            name="formMsg"
-            id="formMsg"
-            onChange={(event) => setMessage(event.target.value)}
-          />
-          <button>Send</button>
-          {!emailValid && <p>Please enter a valid email.</p>}
-          {!messageValid && <p>Message needs to be between 10-250 characters.</p>}
+          <div>
+            <label htmlFor="formEmail">Email</label>
+            <input
+              type="email"
+              name="formEmail"
+              id="formEmail"
+              onChange={(event) => setEmail(event.target.value)}
+            />
+            {!emailValid && <p>Please enter a valid email.</p>}
+          </div>
+          <div>
+            <label htmlFor="formMsg">Message</label>
+            <input
+              type="text"
+              name="formMsg"
+              id="formMsg"
+              onChange={(event) => setMessage(event.target.value)}
+            />
+            {!messageValid && <p>Message needs to be between 10-250 characters.</p>}
+          </div>
+          <button disabled={!formValid}>Send</button>
         </form>
       </div>
     </section>
