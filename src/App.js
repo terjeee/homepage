8591e615ab8d1f777d6cdb7bf8e8faab8353865b
terjeee <96795863+terjeee@users.noreply.github.com>
2022-10-22 22:35:29 +0200

    +contact-success

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import Nav from "./layout/Nav/Nav";
 import Home from "./routes/Home/Home";
 import Contact from "./routes/Contact/Contact";
+import ContactSuccess from "./routes/Contact/ContactSuccess";
 import Experiment from "./routes/Experiment/Experiment";
 import Footer from "./layout/Footer/Footer";
 
@@ -13,6 +14,7 @@ function App() {
       <Routes>
         <Route path="/home" element={<Home />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="/contact-success" element={<ContactSuccess />} />
         <Route path="/sandbox" element={<Experiment />} />
         <Route path="*" element={<Navigate to="/home" />} />
       </Routes>
---
+++ src/routes/Contact/ContactSuccess.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function ContactSuccess() {
+  return <div>ContactSuccess</div>;
+}
