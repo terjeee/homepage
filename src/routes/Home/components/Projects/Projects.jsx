05116c347f1c5fd3442ec336309af1e8737b0e63
terjeee <96795863+terjeee@users.noreply.github.com>
2022-10-28 04:18:57 +0200

    +responsive typewrite, endret størrelse

--- src/routes/Home/components/Projects/Projects.jsx
+++ src/routes/Home/components/Projects/Projects.jsx
@@ -11,18 +11,18 @@ export default function Projects() {
       <div className={css.container}>
         <TypeAnimation
           sequence={[
-            "Prosjekter",
-            5000,
-            "проекты",
-            5000,
-            "progetti",
-            5000,
+            // "Prosjekter",
+            // 5000,
+            // "проекты",
+            // 5000,
+            // "progetti",
+            // 5000,
             "プロジェクト",
             5000,
-            "项目",
-            5000,
-            "المشاريع",
-            5000,
+            // "项目",
+            // 5000,
+            // "المشاريع",
+            // 5000,
           ]}
           wrapper="h1"
           speed="1"
@@ -31,7 +31,6 @@ export default function Projects() {
           style={{
             fontFamily: "inherit",
             color: "inherit",
-            letterSpacing: "7.5px",
           }}
         />
         <TechnologiesList />
