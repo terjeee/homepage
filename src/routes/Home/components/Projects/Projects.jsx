0e65f8db611b1234b35962e9e1e1a9a75d1fa33a
terjeee <96795863+terjeee@users.noreply.github.com>
2022-10-29 06:37:45 +0200

    :)

--- src/routes/Home/components/Projects/Projects.jsx
+++ src/routes/Home/components/Projects/Projects.jsx
@@ -11,18 +11,18 @@ export default function Projects() {
       <div className={css.container}>
         <TypeAnimation
           sequence={[
-            // "Prosjekter",
-            // 5000,
-            // "проекты",
-            // 5000,
-            // "progetti",
-            // 5000,
+            "Prosjekter",
+            5000,
+            "проекты",
+            5000,
+            "progetti",
+            5000,
             "プロジェクト",
             5000,
-            // "项目",
-            // 5000,
-            // "المشاريع",
-            // 5000,
+            "项目",
+            5000,
+            "المشاريع",
+            5000,
           ]}
           wrapper="h1"
           speed="1"
