906bb727762c42a92730e1161f6a242e4517b0c4
terjeee <96795863+terjeee@users.noreply.github.com>
2022-11-22 01:27:55 +0100

    BtnScrollTop vises bare etter yLocation 100

--- src/components/BtnScrollTop.jsx
+++ src/components/BtnScrollTop.jsx
@@ -1,11 +1,23 @@
+import { useEffect, useState } from "react";
 import { handleScrollToTop } from "../utils/functions/helpers";
 import UpArrow from "@mui/icons-material/KeyboardDoubleArrowUp";
 import css from "./BtnScrollTop.module.scss";
 
 export default function BtnScrollTop() {
+  const [yLocation, setYLocation] = useState(window.pageYOffset);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => setYLocation(window.pageYOffset));
+    return window.removeEventListener("scroll", () => setYLocation(window.pageYOffset));
+  }, []);
+
   return (
-    <button className={css.btn} onClick={handleScrollToTop}>
-      <UpArrow />
-    </button>
+    <>
+      {yLocation > 100 && (
+        <button className={css.btn} onClick={handleScrollToTop}>
+          <UpArrow />
+        </button>
+      )}
+    </>
   );
 }
