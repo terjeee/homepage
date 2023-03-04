25c9bd2f3c7a29ee812bf252b9eb13a596ce9320
terjeee <96795863+terjeee@users.noreply.github.com>
2023-03-04 04:55:09 +0100

    +ny rad

--- src/routes/Home/components/TechnologiesList/TechnologiesList.jsx
+++ src/routes/Home/components/TechnologiesList/TechnologiesList.jsx
@@ -1,6 +1,16 @@
 import { useState } from "react";
 
-import { IconJs, IconTs, IconSass, IconReact } from "../../../../assets/icons/index";
+import {
+  IconJs,
+  IconTs,
+  IconSass,
+  IconReact,
+  IconTailwind,
+  IconSvelte,
+  IconNode,
+  IconNext,
+  IconMongoDb,
+} from "../../../../assets/icons/index";
 import { IconRedux, IconRouter } from "../../../../assets/icons/index";
 
 import css from "./TechnologiesList.module.scss";
@@ -13,23 +23,39 @@ export default function TechnologiesList() {
   };
 
   return (
-    <ul className={css.icons}>
-      <li className={css.toggleIcon} onClick={toggleJs}>
-        {showJs && <IconJs />}
-        {!showJs && <IconTs />}
-      </li>
-      <li>
-        <IconSass />
-      </li>
-      <li>
-        <IconReact />
-      </li>
-      <li>
-        <IconRedux />
-      </li>
-      <li>
-        <IconRouter />
-      </li>
-    </ul>
+    <div className={css.container}>
+      <ul className={css.icons_col_one}>
+        <li className={css.toggleIcon} onClick={toggleJs}>
+          {showJs && <IconJs />}
+          {!showJs && <IconTs />}
+        </li>
+        <li>
+          <IconReact />
+        </li>
+        <li>
+          <IconNext />
+        </li>
+        <li>
+          <IconSvelte />
+        </li>
+      </ul>
+      <ul className={css.icons_col_two}>
+        <li>
+          <IconTailwind />
+        </li>
+        <li>
+          <IconSass />
+        </li>
+        <li>
+          <IconRedux />
+        </li>
+        <li>
+          <IconNode />
+        </li>
+        <li>
+          <IconMongoDb />
+        </li>
+      </ul>
+    </div>
   );
 }
