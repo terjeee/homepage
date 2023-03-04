0d10d8ba01963ac6f42d71c9bbcf15324bff7060
terjeee <96795863+terjeee@users.noreply.github.com>
2023-03-04 04:55:36 +0100

    +<title />, sizing

--- src/assets/icons/index.jsx
+++ src/assets/icons/index.jsx
@@ -1,8 +1,13 @@
 export { default as IconJs } from "./IconJs";
 export { default as IconTs } from "./IconTs";
+export { default as IconNode } from "./IconNode";
+export { default as IconMongoDb } from "./IconMongoDb";
+export { default as IconReact } from "./IconReact";
+export { default as IconNext } from "./IconNext";
+export { default as IconSvelte } from "./IconSvelte";
+export { default as IconTailwind } from "./IconTailwind";
+export { default as IconSass } from "./IconSass";
 export { default as IconRedux } from "./IconRedux";
 export { default as IconRouter } from "./IconRouter";
-export { default as IconReact } from "./IconReact";
-export { default as IconSass } from "./IconSass";
 export { default as IconRepository } from "./IconRepository";
 export { default as IconDots } from "./IconDots";
